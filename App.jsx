5dd835f7896c94230686825b8ff06d77162590a6
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-02-26 18:18:05 -0700

    Add link to burger app

--- App.jsx
+++ App.jsx
@@ -1,15 +1,30 @@
 
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Site2025009 from "./sites/2025009/index";
 
+function Home() {
+    return (
+        <>
+            <div className="year" id="2025" style={{ fontSize: "20rem" }}>
+                <ul>
+                    <li>
+                        <Link to="/2025009">2025-009-burger-site</Link>
+                    </li>
+                </ul>
+            </div>
+
+        </>
+    );
+}
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Site2025009 />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/2025009" element={<Site2025009 />} />
                 </Routes>
             </BrowserRouter>
         </>
